133cb238186411f97e4197114ab04dcf5f4a47f3
wujing <jing.woo@outlook.com>
2020-12-19 21:37:12 +0800

    feat: add cmake modules & doxyfile
    Signed-off-by: wujing <jing.woo@outlook.com>

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -3,6 +3,9 @@ option(BUILD_EXAMPLES    "Build examples"             OFF)
 option(GEN_DOCS          "Generate documentation"     OFF)
 option(ENABLE_COVERAGE   "Enable code coverage"       OFF)
 option(ENABLE_DEBUG      "Enable debug mode"          ON)
+# Shared/Dynamic or Static library
+option(BUILD_SHARED_LIBS "Build shared libraries"     ON)
+
 if (ENABLE_DEBUG STREQUAL "ON")
      add_definitions("-g -O2")
 endif()
